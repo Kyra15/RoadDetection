e6553c17c718af9ff7f0708f0a2cd8700000c171
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-03-15 08:45:50 -0400

    Update processing.py

--- processing.py
+++ processing.py
@@ -23,57 +23,26 @@ def process(image):
 
     warped_img = warping(image, mask_reshaped, og_verts)
 
-    chroma_img = detect_white_yellow(warped_img)
+    yellow = detect_yellow(warped_img)
+    white = detect_white(warped_img)
 
-    gray_image = cv2.cvtColor(chroma_img, cv2.COLOR_RGB2GRAY)
+    colored_img = yellow + white
+    cv2.imshow("color", colored_img)
+
+    gray_image = cv2.cvtColor(colored_img, cv2.COLOR_RGB2GRAY)
 
     # filtered_img = filters(warped_img)
 
     cv2.polylines(image, np.array([[p1, p2, p3, p4]], np.int32), True, (0, 255, 0), 8)
 
-    crop_l = gray_image[0:height, width//8:width // 3]
-    crop_r = gray_image[0:height, round(width*0.6):round(width*0.9)]
-    cv2.imshow("crop", crop_r)
+    crop_l = gray_image[0:height, 0:round(width*0.3)]
+    crop_r = gray_image[0:height, round(width*0.6):width]
+    cv2.imshow("cropL", crop_l)
+    cv2.imshow("cropR", crop_r)
 
-    # find the contours on the image
-    contours, hierarchy = cv2.findContours(crop_r, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # contours = imutils.grab_contours((contours, hierarchy))
+    contouring(crop_l, warped_img)
+    contouring(crop_r, warped_img, round(width*0.6))
 
-    # sort the list of contours by the contour area
-    new = list(contours)
-    new.sort(key=cv2.contourArea)
-
-    # if there are at least 2 contours that have been detected
-    if len(new) > 1:
-        # get the 2 largest contours
-        c1 = new[-1]
-        c2 = new[-2]
-
-        # fit polylines to each contour
-        outline1 = cv2.approxPolyDP(c1, 4, True)
-        cv2.drawContours(image, [outline1], -1, (0, 255, 255), 15)
-
-        outline2 = cv2.approxPolyDP(c2, 4, True)
-        cv2.drawContours(image, [outline2], -1, (0, 255, 255), 15)
-
-        c1x1 = 0
-        c1y1 = 0
-        c1x2 = 0
-        c1y2 = 0
-
-        try:
-            M1 = cv2.moments(c1)
-            c1x1 = int(M1['m10']/M1['m00']) + round(width*0.6)
-            c1y1 = int(M1['m01'] / M1['m00'])
-
-            M2 = cv2.moments(c2)
-            c1x2 = int(M2['m10'] / M2['m00']) + round(width*0.6)
-            c1y2 = int(M2['m01'] / M2['m00'])
-        except Exception as e:
-            print(e)
-
-        cv2.circle(warped_img, (c1x1, c1y1), 1, (0, 0, 255), 5)
-        cv2.circle(warped_img, (c1x2, c1y2), 1, (255, 0, 0), 5)
 
         # # draw a midline by going through the polyline and averaging each x and y coordinate
         # # append this averaged coordinate to a list and turn that list into a numpy array
