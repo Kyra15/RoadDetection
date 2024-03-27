67bfdab8745d85056920ba9c9863b305470c23d4
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-03-27 08:44:54 -0400

    Update processing.py

--- processing.py
+++ processing.py
@@ -18,9 +18,6 @@ def process(image):
 
     og_verts = np.float32([[0, height], [0, 0], [width, 0], [width, height]])
 
-    # apply the filters onto the given frame
-    # filtered_img = filters(image)
-
     warped_img = warping(image, mask_reshaped, og_verts)
 
     yellow = detect_yellow(warped_img)
@@ -29,34 +26,42 @@ def process(image):
     colored_img = yellow + white
     cv2.imshow("color", colored_img)
 
-    gray_image = cv2.cvtColor(colored_img, cv2.COLOR_RGB2GRAY)
+    # apply the filters onto the given frame
+    filtered_img = filters(colored_img)
+    cv2.imshow("filter", filtered_img)
 
-    # filtered_img = filters(warped_img)
-
-    cv2.polylines(image, np.array([[p1, p2, p3, p4]], np.int32), True, (0, 255, 0), 8)
-
-    crop_l = gray_image[0:height, 0:round(width*0.3)]
-    crop_r = gray_image[0:height, round(width*0.6):width]
+    crop_l = filtered_img[0:height, 0:round(width * 0.3)]
+    crop_r = filtered_img[0:height, round(width * 0.6):width]
     cv2.imshow("cropL", crop_l)
     cv2.imshow("cropR", crop_r)
 
     contouring(crop_l, warped_img)
-    contouring(crop_r, warped_img, round(width*0.6))
+    contouring(crop_r, warped_img, round(width * 0.6))
 
+    # lines = detect_hough(filtered_img)
+    #
+    # lineys = []
+    #
+    # if lines is not None:
+    #     for i in lines:
+    #         lin1 = cv2.line(warped_img, i[0], i[1], (0, 0, 0), 8)
 
-        # # draw a midline by going through the polyline and averaging each x and y coordinate
-        # # append this averaged coordinate to a list and turn that list into a numpy array
-        # midline = []
-        #
-        # for pt1, pt2 in zip(outline1[:int(len(outline1) / 1.8)], outline2[:int(len(outline2) / 1.8)]):
-        #     mid_x = int((pt1[0][0] + pt2[0][0]) / 2)
-        #     mid_y = int((pt1[0][1] + pt2[0][1]) / 2)
-        #     midline.append([[mid_x, mid_y]])
+    # cv2.polylines(warped_img, np.array([[p1, p2, p3, p4]], np.int32), True, (0, 255, 0), 8)
 
-        # midline = np.array(midline, dtype=np.int32)
-
-        # # draw a polyline from the numpy array onto the frame
-        # cv2.polylines(image, [midline], False, (0, 255, 0), 15)
+    # if len(lineys) > 1:
+    #     # draw a midline by going through the polyline and averaging each x and y coordinate
+    #     # append this averaged coordinate to a list and turn that list into a numpy array
+    #     midline = []
+    #
+    #     for pt1, pt2 in zip(lineys[0][:int(len(lineys[0]) / 1.8)], lines[1][:int(len(lines[1]) / 1.8)]):
+    #         mid_x = int((pt1[0][0] + pt2[0][0]) / 2)
+    #         mid_y = int((pt1[0][1] + pt2[0][1]) / 2)
+    #         midline.append([[mid_x, mid_y]])
+    #
+    #     midline = np.array(midline, dtype=np.int32)
+    #
+    #     # draw a polyline from the numpy array onto the frame
+    #     cv2.polylines(warped_img, [midline], False, (0, 255, 0), 15)
 
 
     # # apply the mask onto the given frame23456u
