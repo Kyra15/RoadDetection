722c0f840ec496387448e92c9de5fae0857212de
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-03-27 08:44:40 -0400

    Update filters.py

--- filters.py
+++ filters.py
@@ -6,9 +6,10 @@ import numpy as np
 # applies a grayscale filter, gaussian blur, and canny image filter on a frame given and returns the final frame
 def filters(img):
     gray_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray_image, (9, 9), 0)
-    cannyimg = cv2.Canny(blur, 200, 300, apertureSize=5)
-    cv2.imshow("can", cannyimg)
+    blur = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    # sobelxy = cv2.Sobel(src=blur, ddepth=cv2.CV_8U, dx=1, dy=1, ksize=5)
+    # cv2.imshow("sobel", sobelxy)
+    cannyimg = cv2.Canny(blur, 150, 200, apertureSize=3, L2gradient=False)
     return cannyimg
 
 
@@ -19,27 +20,15 @@ def warping(img, vertices, og):
     return result
 
 
-def chroma_key(image):
-    up_bound = np.array([210, 200, 180])
-    lw_bound = np.array([100, 110, 100])
-
-    mask = cv2.inRange(image, lw_bound, up_bound)
-    cv2.imshow("masking", mask)
-    res = cv2.bitwise_and(image, image, mask=mask)
-
-    f = image - res
-    cv2.imshow("chroma", f)
-
-    return f
-
-
 def detect_white(image):
+    bright = cv2.addWeighted(image, 1.8, np.zeros(image.shape, image.dtype), 0, -150)
+    cv2.imshow("bright", bright)
     up_bound = np.array([255, 255, 255])
     lw_bound = np.array([175, 175, 175])
 
-    mask = cv2.inRange(image, lw_bound, up_bound)
+    mask = cv2.inRange(bright, lw_bound, up_bound)
     cv2.imshow("masking", mask)
-    res = cv2.bitwise_and(image, image, mask=mask)
+    res = cv2.bitwise_and(bright, bright, mask=mask)
 
     cv2.imshow("white", res)
     return res
@@ -57,8 +46,9 @@ def detect_yellow(image):
     cv2.imshow("yellow", res)
     return cv2.cvtColor(res, cv2.COLOR_HSV2RGB)
 
+
 def detect_hough(image):
-    lines = cv2.HoughLines(image, 1, np.pi / 180, 50)
+    lines = cv2.HoughLines(image, 1, np.pi / 180, 100)
 
     line_coords = []
 
@@ -128,38 +118,26 @@ def contouring(image, final, extra_pix=0):
         # outline3 = cv2.approxPolyDP(c3, 4, True)
         # cv2.drawContours(final, [outline3], -1, (255, 0, 255), 15)
 
-        cx1 = 0
-        cy1 = 0
-        cx2 = 0
-        cy2 = 0
-        cx3 = 0
-        cy3 = 0
+        M1 = cv2.moments(c1)
+        M2 = cv2.moments(c2)
+        M3 = cv2.moments(c3)
 
-        try:
-            M1 = cv2.moments(c1)
+        if M1['m00'] != 0 and M2['m00'] != 0 and M3['m00'] != 0:
             cx1 = int(M1['m10'] / M1['m00']) + extra_pix
             cy1 = int(M1['m01'] / M1['m00'])
 
-            M2 = cv2.moments(c2)
             cx2 = int(M2['m10'] / M2['m00']) + extra_pix
             cy2 = int(M2['m01'] / M2['m00'])
 
-            M3 = cv2.moments(c3)
             cx3 = int(M3['m10'] / M3['m00']) + extra_pix
             cy3 = int(M3['m01'] / M3['m00'])
-        except Exception as e:
-            pass
 
-        cv2.circle(final, (cx1, cy1), 1, (0, 0, 255), 5)
-        cv2.circle(final, (cx2, cy2), 1, (255, 0, 0), 5)
-        cv2.circle(final, (cx3, cy3), 1, (255, 255, 0), 5)
+            cv2.circle(final, (cx1, cy1), 1, (0, 0, 255), 5)
+            cv2.circle(final, (cx2, cy2), 1, (255, 0, 0), 5)
+            cv2.circle(final, (cx3, cy3), 1, (255, 255, 0), 5)
 
-        midx = (cx1+cx2+cx3)/3
-        mid
-        # get mid and then height and then drw lies
-        
-        
-        # poly = np.array([[cx1, cy1], [cx2, cy2], [cx3, cy3]])
-        # approx = cv2.approxPolyDP(poly, 4, False)
-        # cv2.drawContours(final, [approx], -1, (0, 0, 255), 3)
+            poly = np.array([[cx1, cy1], [cx2, cy2], [cx3, cy3]])
+            approx = cv2.approxPolyDP(poly, 4, False)
+            cv2.drawContours(final, [approx], -1, (0, 0, 255), 3)
 
+            # find slope of lines created and then continue the lines?
