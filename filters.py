9dd4af8376b4f0dbcd2c67c59b0ddc128f909a81
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-04-01 23:14:21 -0400

    Update filters.py

--- filters.py
+++ filters.py
@@ -2,66 +2,45 @@
 import cv2
 import numpy as np
 import math
-import time
 
 
-# applies a grayscale filter, gaussian blur, and canny image filter on a frame given and returns the final frame
-def filters(img):
-    gray_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray_image, (5, 5), 0)
-    cannyimg = cv2.Canny(blur, 150, 200, apertureSize=5, L2gradient=False)
-    return cannyimg
-
-
+# warps an image given a set a vertices
 def warping(img, mask_vert, screen_vert):
     matrixy = cv2.getPerspectiveTransform(mask_vert, screen_vert)
     result = cv2.warpPerspective(img, matrixy, (img.shape[1], img.shape[0]))
-    # cv2.imshow("warp", result)
     return result
 
 
+# un-warps an image given a set of vertices
 def unwarped(img, mask_vert, screen_vert):
     matrix2 = cv2.getPerspectiveTransform(screen_vert, mask_vert)
     result = cv2.warpPerspective(img, matrix2, (img.shape[1], img.shape[0]))
-    # cv2.imshow("unwarp", result)
     return result
 
 
+# detects the HoughLines within a frame
 def detect_hough(image):
     lines = cv2.HoughLines(image, 1, np.pi / 180, 100)
-
     line_coords = []
 
     if lines is not None:
         # The below for loop runs till r and theta values
         # are in the range of the 2d array
         for r_theta in lines:
+            # perform calculations onto the returned polar coordinates to convert them into cartesian ones
+
             arr = np.array(r_theta[0], dtype=np.float64)
             r, theta = arr
-            # Stores the value of cos(theta) in a
             a = np.cos(theta)
-
-            # Stores the value of sin(theta) in b
             b = np.sin(theta)
-
-            # x0 stores the value rcos(theta)
             x0 = a * r
-
-            # y0 stores the value rsin(theta)
             y0 = b * r
-
-            # x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
             x1 = int(x0 + 1000 * (-b))
-
-            # y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
             y1 = int(y0 + 1000 * (a))
-
-            # x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
             x2 = int(x0 - 1000 * (-b))
-
-            # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
             y2 = int(y0 - 1000 * (a))
 
+            # get the slope of each of the HoughLines returned and append those if they are not horizontal
             try:
                 slope = round((y2 - y1) / (x2 - x1))
             except:
@@ -70,14 +49,19 @@ def detect_hough(image):
             if not (-1 < slope < 1):
                 line_coords.append([(x1, y1), (x2, y2)])
 
+        # return the final list of HoughLines
         return line_coords
 
 
-def check_hough(good_lines, img, og, extra_pix=0):
+# check and filter the given HoughLines
+def check_hough(good_lines, og, extra_pix=0):
     good_dist = []
     good = []
     mid_line = []
 
+    # if the given lines is not empty,
+    # check to make sure each of the lines are at least 20 pixels away from each other
+    # append all the good lines to a seperate list
     if good_lines is not None:
         for i in good_lines:
             x1, y1 = i[0]
@@ -91,13 +75,11 @@ def check_hough(good_lines, img, og, extra_pix=0):
             if not dist:
                 good_dist.append([[x1 + extra_pix, y1], [x2 + extra_pix, y2]])
 
-        # to prevent a divide by zero error, check to make sure that at least 1 line was detected
+        # to prevent a divide by zero error, check to make sure that at least 2 lines were detected
         # calculate the average of two of the endpoints of the outputted lines
+        # draw the final midline and return the original image and midline coordinates
         if good_dist is not None:
-            # print("dist", len(good_dist))
             for i in range(len(good_dist)):
-                # cv2.line(og, (good_dist[i][0][0], good_dist[i][0][1]),
-                # (good_dist[i][1][0], good_dist[i][1][1]), (0, 0, 255), 20, cv2.LINE_AA)
                 good.append(
                     (good_dist[i][0][0], good_dist[i][0][1], good_dist[i][1][0], good_dist[i][1][1]))
             if len(good) >= 2:
@@ -110,37 +92,65 @@ def check_hough(good_lines, img, og, extra_pix=0):
     return og, mid_line
 
 
+# average 2 lines given their start and end coordinates
 def average_lines(line1_start, line1_end, line2_start, line2_end):
-    # Calculate midpoints of each line
+    # calculate midpoints of each line
     line1_midpoint = ((line1_start[0] + line1_end[0]) / 2, (line1_start[1] + line1_end[1]) / 2)
     line2_midpoint = ((line2_start[0] + line2_end[0]) / 2, (line2_start[1] + line2_end[1]) / 2)
 
-    # Average the midpoints
+    # get the average of the midpoints
     avg_midpoint = ((line1_midpoint[0] + line2_midpoint[0]) / 2, (line1_midpoint[1] + line2_midpoint[1]) / 2)
 
+    # calculate the slope of each line
+    # if there is a zero division error, set the slope to some arbitrarily high number
     try:
-        # Calculate slope of each line
-        slope1 = (line1_end[1] - line1_start[1]) / (line1_end[0] - line1_start[0])
+        slope1 = abs((line1_end[1] - line1_start[1]) / (line1_end[0] - line1_start[0]))
     except ZeroDivisionError:
         slope1 = 999
 
     try:
-        slope2 = (line2_end[1] - line2_start[1]) / (line2_end[0] - line2_start[0])
+        slope2 = abs((line2_end[1] - line2_start[1]) / (line2_end[0] - line2_start[0]))
     except ZeroDivisionError:
         slope2 = 999
 
-    # Average the slopes
+    # average the slopes and intercepts
     avg_slope = (slope1 + slope2) / 2
-
-    # Calculate intercept of the averaged line
     avg_intercept = avg_midpoint[1] - avg_slope * avg_midpoint[0]
 
-    # Calculate endpoints of the averaged line
+    # get the endpoints of the averaged line
     x1_avg = min(line1_start[0], line1_end[0], line2_start[0], line2_end[0])
     x2_avg = max(line1_start[0], line1_end[0], line2_start[0], line2_end[0])
     y1_avg = avg_slope * x1_avg + avg_intercept
     y2_avg = avg_slope * x2_avg + avg_intercept
 
-    return [round(x1_avg), round(y1_avg)], [round(x2_avg), round(y2_avg)]
+    # return the coordinates for the average line
+    return (round(x1_avg), round(y1_avg)), (round(x2_avg), round(y2_avg))
+
+
+# using template matching, look for a left turn arrow that matches a previously stored arrow
+# if that is found, display the word "left"
+def detect_arrow_left(frame):
+    frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('left_turn_temp.png', 0)
+    res = cv2.matchTemplate(frame_bw, template, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where(res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.putText(frame, "left", (50, 100), fontFace=1, fontScale=8.0, color=(0, 0, 0), thickness=8)
+    return frame
+
+
+# using template matching, look for a right turn arrow that matches a previously stored arrow
+# if that is found, display the word "right"
+def detect_arrow_right(frame):
+    frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('right_turn_temp.png', 0)
+    res = cv2.matchTemplate(frame_bw, template, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where(res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.putText(frame, "right", (50, 100), fontFace=1, fontScale=8.0, color=(0, 0, 0), thickness=8)
+    return frame
+
 
 
